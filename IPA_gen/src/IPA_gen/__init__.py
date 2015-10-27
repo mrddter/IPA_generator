b9e62d5780f8f6b6c345c8bb3bcaf8c9f69380ba
mphilli <mphilli5@oswego.edu>
2015-10-27 16:08:08 -0400

    update

--- IPA_gen/src/IPA_gen/__init__.py
+++ IPA_gen/src/IPA_gen/__init__.py
@@ -1,7 +1,10 @@
-# Converts English Text into IPA format. 
+# -*- coding: utf-8 -*-
 
-import gen_mods                  
+if __name__ == '__main__':
+    pass
+# Converts English Text into IPA format. 
+import gen_mods
 print('English Text to IPA Converter\n')
 userInput = input('[English]: ').split()
 IPA_CMU = gen_mods.getIPA_CMU(userInput)
-gen_mods.get_final(IPA_CMU)
+print(gen_mods.get_final(IPA_CMU))
--- IPA_gen/src/gen_mods.py
+++ IPA_gen/src/gen_mods.py
@@ -1,18 +1,11 @@
-import os 
 import re 
 
-# file location may vary! 
-os.chdir(r'C:\Users\Mike\git\IPA_gen')
-file_object = open('CMU_phonetic_dictionary.txt', 'r+')  
+file_object = open("CMU_phonetic_dictionary.txt", 'r+')  
 
-
-words = []
-element = []
+words = []; element = []
 
 def getIPA_CMU(userInput):
-    IPA_CMUT = ""
-    ele_num = 0
-    mult_num = []
+    IPA_CMUT = ""; ele_num = 0; mult_num = []
     for w in userInput:
         if w == "a":
             IPA_CMUT += "a "
@@ -33,16 +26,11 @@ def getIPA_CMU(userInput):
                 IPA_CMU = re.sub("[0-9]", "", IPA_CMU)
                 IPA_CMUT += IPA_CMU + " "
             if w2 in string:
-                multiple = []
-                mult_num = []
+                multiple = []; mult_num = []
                 mult_num.append(ele_num)
                 multiple.append(string)
                 get_multiple(multiple,w2)
-                # print('multiple possibilities found:')
                 multiple = get_IPA(get_multiple(multiple,w2))
-                #get_final(IPA_CMUT.replace(IPA_CMUT[ele_num], multiple))
-                
-                #multiple pronunciations of w2
             if w == string:
                 print('here')          
     aslist = IPA_CMUT.split()
@@ -52,8 +40,7 @@ def getIPA_CMU(userInput):
             old = aslist[m-1]
             aslist[m-1] = aslist[m-1].replace(old, multiple) 
             asliststring = " ".join(aslist)
-            get_final(asliststring)
-            
+            get_finalPrint(asliststring)     
     return IPA_CMUT; 
 
 def multIPA(word, IPA):
@@ -76,57 +63,42 @@ def get_IPA(IPA_CMU):
     IPA = "" # final string
     IPA_CMU = IPA_CMU.split()
     for w in IPA_CMU:
-        if w == "a":
-            w = w.replace("a", "ə")
-        if "ey" in w:
-            w = w.replace("ey", "e")
-        if "aa" in w:
-            w = w.replace("aa", "ɑ")    
-        if "ae" in w:
-            w = w.replace("ae", "æ")
-        if "ah" in w:
-            w = w.replace("ah", "ə")
-        if "ao" in w: 
-            w = w.replace("ao", "ɔ")
-        if "aw" in w: 
-            w = w.replace("aw", "aʊ")
-        if "ay" in w: 
-            w = w.replace("ay", "aɪ")
-        if "ch" in w: 
-            w = w.replace("ch", "ʧ")
-        if "dh" in w: 
-            w = w.replace("dh", "ð")
-        if "eh" in w: 
-            w = w.replace("eh", "ɛ")
-        if "er" in w: 
-            w = w.replace("er", "ər")
-        if "hh" in w: 
-            w = w.replace("hh", "h")
-        if "ih" in w: 
-            w = w.replace("ih", "ɪ")
-        if "jh" in w: 
-            w = w.replace("jh", "ʤ")
-        if "ng" in w: 
-            w = w.replace("ng", "ŋ")
-        if "ow" in w: 
-            w = w.replace("ow", "oʊ")
-        if "oy" in w: 
-            w = w.replace("oy", "ɔɪ")
-        if "sh" in w: 
-            w = w.replace("sh", "ʃ")
-        if "th" in w: 
-            w = w.replace("th", "θ")
-        if "uh" in w: 
-            w = w.replace("uh", "ʊ")
-        if "uw" in w: 
-            w = w.replace("uw", "u")
-        if "zh" in w: 
-            w = w.replace("zh", "ʒ")
-        if "iy" in w: 
-            w = w.replace("iy", "i")
+        if w == "a":   w = w.replace("a", "ə")
+        if "ey" in w:  w = w.replace("ey", "e")
+        if "aa" in w:  w = w.replace("aa", "ɑ")    
+        if "ae" in w:  w = w.replace("ae", "æ")
+        if "ah" in w:  w = w.replace("ah", "ə")
+        if "ao" in w:  w = w.replace("ao", "ɔ")
+        if "aw" in w:  w = w.replace("aw", "aʊ")
+        if "ay" in w:  w = w.replace("ay", "aɪ")
+        if "ch" in w:  w = w.replace("ch", "ʧ")
+        if "dh" in w:  w = w.replace("dh", "ð")
+        if "eh" in w:  w = w.replace("eh", "ɛ")
+        if "er" in w:  w = w.replace("er", "ər")
+        if "hh" in w:  w = w.replace("hh", "h")
+        if "ih" in w:  w = w.replace("ih", "ɪ")
+        if "jh" in w:  w = w.replace("jh", "ʤ")
+        if "ng" in w:  w = w.replace("ng", "ŋ")
+        if "ow" in w:  w = w.replace("ow", "oʊ")
+        if "oy" in w:  w = w.replace("oy", "ɔɪ")
+        if "sh" in w:  w = w.replace("sh", "ʃ")
+        if "th" in w:  w = w.replace("th", "θ")
+        if "uh" in w:  w = w.replace("uh", "ʊ")
+        if "uw" in w:  w = w.replace("uw", "u")
+        if "zh" in w:  w = w.replace("zh", "ʒ")
+        if "iy" in w:  w = w.replace("iy", "i")
+        if "y" in w:   w = w.replace("y", "j")
         IPA += w + " "
     return(IPA.strip());
 
 def get_final(IPA_CMU):
+    final_IPA = '[IPA]: [' + get_IPA(IPA_CMU) + ']'
+    return final_IPA;
+
+def get_finalRAW(IPA_CMU):
+    final_IPA_RAW = get_IPA(IPA_CMU)
+    return final_IPA_RAW;
+
+def get_finalPrint(IPA_CMU):
     final_IPA = '[IPA]: [' + get_IPA(IPA_CMU) + ']'
     print(final_IPA)
